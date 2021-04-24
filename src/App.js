db58f13915bac603b9378f51cd2fdba77bb8a358
ludchieng <lud.chieng@hotmail.fr>
2021-04-24 18:01:39 +0200

    Change API

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import throttledQueue from 'throttled-queue';
 import './App.css';
 import fetchExchangeRates from'./StonkFetcher/stonkfetcher';
 
 function App() {
-  const [data, setData] = useState([]);
-  const throttle = throttledQueue(1, 1000) // at most 1 request every second
-  
+  const [data, setData] = useState();
+
   useEffect(() => {
-    if (data.length === 0) {
-      throttle(() => {
-        fetchExchangeRates('BTC', 47, 'hours')
-          .then((resp) => resp.json())
-          .then((data) => {
-            setData(data.map((e) => ({
-              timestamp: e.timestamp,
-              rate: parseFloat(e.rate)
-            })))
-          })
-      })
+    if (data === undefined) {
+      fetchExchangeRates()
+        .then((resp) => resp.json())
+        .then((data) => { setData(data) })
     }
   });
 
   return (
     <div>
       <p>Allo?</p>
-      <p>{ (data.length) ? data[0].rate : "" }</p>
+      <p>{ (data !== undefined) ? data.message.stonkimac : "" }</p>
     </div>
   );
 }
--- src/StonkFetcher/stonkfetcher.js
+++ src/StonkFetcher/stonkfetcher.js
@@ -1,12 +1,5 @@
-import moment from 'moment'
-
-const fetchExchangeRates = async (currency, count, step = 'hours') => {
-  return await fetch(`https://api.nomics.com/v1/exchange-rates/history`
-      + `?key=${process.env.REACT_APP_NOMICS_API_KEY}`
-      + `&currency=${currency}`
-      + `&start=${moment(new Date()).subtract(count, step).toISOString()}`
-      + `&end=${moment(new Date()).toISOString()}`
-      + `&format=json`)
+const fetchExchangeRates = async () => {
+  return await fetch(`https://cryptostonks.ludchieng.fr/exchange-rates`)
 };
 
 export default fetchExchangeRates;

9c0ee1717ada1ea27dc0ae2e927c1fe552e124aa
benjamin <benjamin27briere@orange.fr>
2021-04-25 13:14:30 +0200

    animation begining

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
 
 function App() {
+  
+  const handleLeftSideBarClick = (e, name) => {
+    let temp = document.getElementById(name);
+    console.log(temp);
+  }
+  
   return (
     <div id="LeftSideBar">
       <div id="LeftSideBarCryptoSelector">
-        <div className="LeftSideBarCrypto active">$BTC</div>
+        <div className="LeftSideBarCrypto" onClick = {(event) => handleLeftSideBarClick(event,"$BTC")} >$BTC</div>
         <div className="LeftSideBarCrypto">$ETH</div>
         <div className="LeftSideBarCrypto">$BAT</div>
         <div className="LeftSideBarCrypto">$DAI</div>
       </div>
       <div id="LeftSideBarCryptoCharts">
-        <div className="LeftSideBarCryptoChart">$BTC</div>
-        <div className="LeftSideBarCryptoChart">$ETH</div>
-        <div className="LeftSideBarCryptoChart">$BAT</div>
-        <div className="LeftSideBarCryptoChart">$DAI</div>
+        <div id="$BTC" className="LeftSideBarCryptoChart active">$BTC</div>
+        <div id="$ETH" className="LeftSideBarCryptoChart">$ETH</div>
+        <div id="$BAT" className="LeftSideBarCryptoChart">$BAT</div>
+        <div id="$DAI" className="LeftSideBarCryptoChart">$DAI</div>
       </div>
     </div>
   );

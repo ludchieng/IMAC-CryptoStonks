c2fd7a7e3b610c07b8549643ff23b0d6d6a2832b
benjamin <benjamin27briere@orange.fr>
2021-04-25 10:30:57 +0200

    buttons first try

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import './App.css';
 
 function App() {
   return (
-    <p>Allo?</p>
+    <div id="LeftSideBar">
+      <div id="LeftSideBarCryptoSelector">
+        <div className="LeftSideBarCrypto active">$BTC</div>
+        <div className="LeftSideBarCrypto">$ETH</div>
+        <div className="LeftSideBarCrypto">$BAT</div>
+        <div className="LeftSideBarCrypto">$DAI</div>
+      </div>
+      <div id="LeftSideBarCryptoCharts">
+        <div className="LeftSideBarCryptoChart">$BTC</div>
+        <div className="LeftSideBarCryptoChart">$ETH</div>
+        <div className="LeftSideBarCryptoChart">$BAT</div>
+        <div className="LeftSideBarCryptoChart">$DAI</div>
+      </div>
+    </div>
   );
 }
 

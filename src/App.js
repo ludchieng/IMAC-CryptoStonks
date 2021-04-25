3f4d06d29621ec963a32a1f12b9e18ff01dcd9b0
benjamin <benjamin27briere@orange.fr>
2021-04-25 13:52:37 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import { render } from '@testing-library/react';
+import { useState } from 'react';
 import './App.css';
 import LeftSideBar from "./LeftSideBar"
 
+let cryptoList=[
+  {name : "ImacCoin", price : 10, symbol : "$IMA"}, 
+  {name : "SaltCoin", price : 100, symbol : "$SLT"}]
+
+
 function App() {
-  <LeftSideBar/>
+  
+  const [crypto, setCrypto] = useState(cryptoList);
+
+  return(
+    <LeftSideBar
+      cryptoList = {crypto}
+    />
+  )
 }
 
 export default App;
--- src/LeftSideBar.js
+++ src/LeftSideBar.js
@@ -2,25 +2,25 @@ import React from 'react';
 
 class LeftSideBar extends React.Component {
 
-    handleLeftSideBarClick = (name) => {
-        let temp = document.getElementById(name);
+    constructor(props){
+        super(props);
+        this.cryptoList = props.cryptoList;
+    }
+
+    handleLeftSideBarClick = (s) => {
+        let temp = document.getElementById(s);
         console.log(temp);
     }
 
     render() {
         return(
             <div id="LeftSideBar">
+                {console.log(this.cryptoList)}
                 <div id="LeftSideBarCryptoSelector">
-                    <div className="LeftSideBarCrypto" onClick = { handleLeftSideBarClick => ("$BTC")} >$BTC</div>
-                    <div className="LeftSideBarCrypto">$ETH</div>
-                    <div className="LeftSideBarCrypto">$BAT</div>
-                    <div className="LeftSideBarCrypto">$DAI</div>
+                    {this.cryptoList.map( (e) => (<div className="LeftSideBarCrypto" onClick = { handleLeftSideBarClick => (e.symbol)} > {e.symbol} {e.price}</div>))}
                 </div>
                 <div id="LeftSideBarCryptoCharts">
-                    <div id="$BTC" className="LeftSideBarCryptoChart active">$BTC</div>
-                    <div id="$ETH" className="LeftSideBarCryptoChart">$ETH</div>
-                    <div id="$BAT" className="LeftSideBarCryptoChart">$BAT</div>
-                    <div id="$DAI" className="LeftSideBarCryptoChart">$DAI</div>
+                    {this.cryptoList.map( (e) => (<div className="LeftSideBarCryptoChart" onClick = { handleLeftSideBarClick => (e.symbol)} > {e.symbol}</div>))}
                 </div>
             </div>
         )
